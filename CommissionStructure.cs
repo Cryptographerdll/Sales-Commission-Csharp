ab76e051eba512f30077b9fc1282bd9f30e1d112
khalil DupGames <85350190+khalilDup@users.noreply.github.com>
2021-09-25 18:00:31 +0100

    Add files via upload

---
+++ CommissionStructure.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesCommission
+{
+    class CommissionStructure
+    {
+        private const decimal Fist_Item = 239.99M;
+        private const decimal Sec_Item = 129.75M;
+        private const decimal Third_Item = 99.95M;
+        private const decimal Fourth_Item = 350.89M;
+        private const decimal Pay_Per_Week = 200M;
+        private const decimal Percentage = 0.09M;
+
+        public CommissionStructure(int item1Sold, int item2Sold, int item3Sold, int item4Sold)
+        {
+            Item1Sold = item1Sold;
+            Item2Sold = item2Sold;
+            Item3Sold = item3Sold;
+            Item4Sold = item4Sold;
+        }
+
+        public int Item1Sold { get; set; }
+        public int Item2Sold { get; set; }
+        public int Item3Sold { get; set; }
+        public int Item4Sold { get; set; }
+
+        public decimal GetTotalSales()
+        {
+            return Fist_Item * Item1Sold + Sec_Item * Item2Sold + Third_Item * Item3Sold + Fourth_Item * Item4Sold;
+        }
+
+        public decimal Commission()
+        {
+            return GetTotalSales() * Percentage + Pay_Per_Week;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SalesCommission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               A large company pays its salespeople on a commission basis.
+               The salespeople receive $200 per week plus 9% of their gross sales for that week. 
+            For example, a salesperson who sells $5,000 worth of merchandise in a week receives $200 plus 9% of $5,000, or a total of $650. 
+            You’ve been supplied with a list of the items sold by each salesperson. The values of these items are as follows:
+                item 1:  239.99
+                item 2:  129.75
+                item 3:  99.95
+                item 4: 350.89
+                Develop a C# app that inputs one salesperson’s items sold for the last week, 
+            then calculates and displays that salesperson's earnings. There’s no limit to the number of items that can be sold by a salesperson.
+             */
+          int item1Sold = 0;
+          int item2Sold = 0;
+          int item3Sold = 0;
+          int item4Sold = 0;
+
+            Console.WriteLine("how many of items1 were sold ");
+            item1Sold = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("how many of items2 were sold ");
+            item2Sold = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("how many of items3 were sold ");
+            item3Sold = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("how many of items4 were sold ");
+            item4Sold = int.Parse(Console.ReadLine());
+
+            CommissionStructure cs = new CommissionStructure(item1Sold, item2Sold, item3Sold, item4Sold);
+
+            // Total sales :
+            Console.Write("Total sales including 200$ : {0:C}", cs.Commission());
+            Console.ReadKey();
+
+        }
+    }
+}
